9eb395b3ca08a5d087eb5c3650cfd2119bcef1ef
Sanai <Sanai.Maity@sparinfosys.com>
2021-04-07 17:27:26 +0530

    filter changed

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -9,7 +9,11 @@ export class HttpService {
 
   constructor(private  httpClient:HttpClient) { }
 
-  getData(): Observable<any>{
-    return this.httpClient.get("http://52.14.186.140:9000/process_and_load_data_api");
+  getData(imagePath): Observable<any>{
+    return this.httpClient.get("http://52.14.186.140:9000/process_and_load_data_api?aws_image_path="+imagePath);
+  }
+
+  getImages(): Observable<any>{
+    return this.httpClient.get("http://52.14.186.140:9000/aws/images");
   }
 }
